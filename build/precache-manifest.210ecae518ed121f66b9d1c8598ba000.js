ca293e5e486492f8e86904a8cccec352162130e6
Ihor Vorobei <50757489+igorekRadisson@users.noreply.github.com>
2020-09-22 18:06:39 +0200

    Add files via upload

---
+++ build/precache-manifest.210ecae518ed121f66b9d1c8598ba000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d89cbb5cb993ccc767398d2062609d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35ea9f88a08303bf006",
+    "url": "/static/css/main.f548a31d.chunk.css"
+  },
+  {
+    "revision": "2558c0a217a12b302a49",
+    "url": "/static/js/2.e10136f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e10136f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35ea9f88a08303bf006",
+    "url": "/static/js/main.a4e68640.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
